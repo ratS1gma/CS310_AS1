10590b31b9e70b65f152026c96aa55f2ec30d64d
Mark Barchuk <markbarchuk@gmail.com>
2026-06-28 14:30:20 +0300

    Add tasks 4-6

--- AS3/AS3.sql
+++ AS3/AS3.sql
@@ -49,3 +49,68 @@ begin
 	end if;
 end;
 $$ language plpgsql;
+
+--task4
+
+create or replace function update_order_total()
+returns trigger
+as $$
+declare 
+	p_order_id int;
+begin
+	if tg_op = 'DELETE' then
+		p_order_id := old.order_id;
+	else
+		p_order_id := new.order_id;
+	end if;
+	
+	update orders
+	set total_amount = calculate_order_total(p_order_id)
+	where order_id = p_order_id;
+
+	if tg_op = 'DELETE' then
+		return old;
+	end if;
+	return new;
+end;
+$$ language plpgsql;
+
+
+create or replace trigger trg_order_itm after insert or update or delete on order_items 
+for each row 
+execute function update_order_total();
+
+
+--task5
+create or replace function insert_logs()
+returns trigger
+as $$
+begin
+	insert into order_log(order_id,customer_id,action,log_date) values
+	(new.order_id,new.customer_id,'create',LOCALTIMESTAMP);
+
+	return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger trg_order after insert on orders
+for each row 
+execute function insert_logs();
+
+--task6
+
+insert into customers(full_name,email,balance) values
+('test test','test@test.com','1000');
+
+insert into products(product_name,price,stock_quantity) values
+('test_p',100,10);
+
+call create_order(5);
+
+call add_product_to_order(4,6,6);
+
+select * from orders where  order_id = 4;
+
+select * from order_items where order_id = 4;
+
+select * from order_log where order_id = 4;
